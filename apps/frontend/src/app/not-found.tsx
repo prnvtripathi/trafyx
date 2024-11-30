d792e0a51818e7daa94ce1e6688d7e19cc484b5c
Ishaan2053 <Ishaan2053@gmail.com>
2024-11-30 10:39:03 +0530

    Add documentation sitemap

--- apps/frontend/src/app/not-found.tsx
+++ apps/frontend/src/app/not-found.tsx
@@ -5,7 +5,7 @@ import React from "react";
 import { Button } from "@/components/ui/button";
 
 export const metadata = {
-  title: "404: Not Found",
+  title: "404: Page Not Found",
 };
 
 function Custom404() {
