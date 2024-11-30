2d6b45ee1b0110977edf3f3ac6f6813b5fd4c3dd
Ishaan2053 <Ishaan2053@gmail.com>
2024-12-01 03:50:50 +0530

    Update @radix-ui/react-alert-dialog to version 1.1.2 and remove unused fetchUsers function

--- apps/frontend/src/lib/data.ts
+++ apps/frontend/src/lib/data.ts
@@ -1,23 +1,6 @@
 import { User } from "./models";
 import { connectToDB } from "./utils";
 
-// Fetch users based on a search query and company ID
-// export const fetchUsers = async (q, companyID) => {
-//   // Create a case-insensitive regular expression for the search query
-//   const regex = new RegExp(q, "i");
-//   try {
-//     connectToDB();
-//     // Count the number of users matching the search criteria
-//     const count = await User.find({ username: { $regex: regex }, companyID }).count();
-//     // Fetch the users matching the search criteria
-//     const users = await User.find({ username: { $regex: regex }, companyID });
-//     return { count, users };
-//   } catch (err) {
-//     console.log(err);
-//     throw new Error("Failed to fetch users!");
-//   }
-// };
-
 // Fetch a single user by ID
 export const fetchUser = async (id: any) => {
   console.log(id);
