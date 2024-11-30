3d451d83749580a7a31c30c75e5b9963fce623a9
Ishaan2053 <Ishaan2053@gmail.com>
2024-12-01 02:02:09 +0530

    Replace custom navigation with Navbar component in documentation layout

--- apps/frontend/src/app/docs/layout.tsx
+++ apps/frontend/src/app/docs/layout.tsx
@@ -1,3 +1,4 @@
+import { Navbar } from "@/components/navigation/navbar";
 import Search from "@/components/navigation/search";
 import { Sidebar } from "@/components/navigation/sidebar";
 import Footer from "@/components/ui/footer";
@@ -12,17 +13,7 @@ export default function Documents({
 }>) {
   return (
     <>
-      {/* <Header /> */}
-      <nav className="flex items-center justify-between shadow-md p-4 sticky top-0 backdrop-blur-sm w-full z-50">
-        <Link href="/" className="flex flex-row space-x-3">
-          {" "}
-          <div className="bg-primary rounded-full shadow-xl text-white  p-1">
-            <GearIcon className=" m-0 h-6 p-0 rounded-full w-full" />
-          </div>
-          <h1 className="font-bold text-2xl">Apilux</h1>
-        </Link>
-        <Search />
-      </nav>
+      <Navbar />
       <div className="flex items-start gap-14 px-3">
         <Sidebar />
         <div className="flex-1 md:flex-[6]">{children}</div>{" "}
